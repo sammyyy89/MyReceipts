264f48beaa7cd4d2014e0fd36534f4447df822da
Saemi An <saemi0809@gmail.com>
2022-11-07 15:01:12 -0500

    Initial Commit

---
+++ MyReceipts/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyReceipts
+//
+//  Created by Saemi An on 11/7/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
